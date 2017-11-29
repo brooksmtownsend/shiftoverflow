86fa1711d451cf97054f5fc3808806624db8eac8
brookebc <brookebc@MacBook-Pro-7.local>
2017-11-28 21:05:34 -0500

    no message

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,23 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Shift Overflow</h2>
         </div>
+
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Test text="This is a property" />
         <Test text="This too" />
+
+        <div className="App-footer">
+          <h3>Created by<br/>
+          Brooks Townsend<br/>
+          Brooke Canter<br/>
+          Helen Qin<br/>
+          Kiet Hyunh</h3>
+
+        </div>
       </div>
     );
   }
