ba06d8e0752cb9d4f56220c7e20a4076e4525b1a
Kit <kthuynh14@gmail.com>
2017-12-04 19:49:49 -0500

    remove day props

--- src/components/CollapsibleHourShift.tsx
+++ src/components/CollapsibleHourShift.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Collapsible, CollapsibleItem } from 'react-materialize'
 
 
-export class CollapsibleHourShift extends React.Component<{day: Date}> {
+export class CollapsibleHourShift extends React.Component<{}> {
 
     constructor(props: any) {
         super(props)
