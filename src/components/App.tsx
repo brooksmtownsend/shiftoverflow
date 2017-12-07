848fc8ce43bcdace931db20c16787adb44b39135
brookebc <brookebc@MacBook-Pro-7.local>
2017-12-06 22:35:18 -0500

    View byWeekly/byday tabs. No on click functionality yet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,11 @@
+// React lets us write HTML-ish tags but we are really just writing
+// XML based object representations
+
+// React uses essentially a fake DOM to be rendered on the server
+// It uses the least amount of DOM manipulation possible in order to 
+// keep your components up to date
+
+
 import * as React from 'react';
 import { AvailabilityDay } from './AvailabilityDay';
 import '../styles/App.css';
@@ -7,13 +15,25 @@ import { Day } from './AvailabilitySquare'
 import { ScheduleDay } from './ScheduleDay';
 import { CollapsibleHourShift } from './CollapsibleHourShift';
 
-class App extends React.Component {
+ // every component has a state object and props object
+//  Props don't change
+//  State starts with default value and mutates
+class App extends React.Component  <{}> {
+
+  isWeekly: boolean;
   constructor(props: any) {
     super(props);
-    this.state = {
-      isWeekly: true,
-    }
+    this.state = {isWeekly: true}
+    this.changeView = this.changeView.bind(this);
   }
+  
+  changeView() {
+    this.setState({isWeekly: !this.isWeekly})
+  }
+  // changeDay() {
+  //   this.setState({isWeekly: false})
+  // }
+
   render() {
     
     return (
@@ -36,13 +56,23 @@ class App extends React.Component {
           <div className='verticalDivide'></div>
           <div className='rightContainer'>
           <h1> Schedule </h1>
-            <div className='rightSide'>     
+<div>
+          <button id='week' onClick={() => this.changeView()}>View Weekly
+          {!this.isWeekly && <div className='rightSide'></div>} 
+            </button>
+            <button id='day' onClick={() => this.changeView()}>View Day
+            {!this.isWeekly && <div className='kit'></div>} 
+            </button>
+     </div>   
+            <div className='rightSide'>
               <ScheduleDay day={Day.Sunday}/>
               <ScheduleDay  day={Day.Monday}/>
               <ScheduleDay  day={Day.Tuesday}/>
               <ScheduleDay  day={Day.Wednesday}/>
               <ScheduleDay  day={Day.Thursday}/>
               <ScheduleDay  day={Day.Friday}/>
+              </div>
+            <div className='kit'>
               <CollapsibleHourShift day={Day.Sunday}/>
             </div>
             {/* <div><CollapsibleHourShift day={Day.Sunday}/></div> */}
@@ -58,6 +88,7 @@ class App extends React.Component {
      </div>
     );
   }
+  
 }
 
 export default App;
