aa8cb5622b66ab2eaee79b3b103447758ffd6f37
brookebc <brookebc@MacBook-Pro-7.local>
2017-12-07 02:32:06 -0500

    preserve

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,6 @@
 // It uses the least amount of DOM manipulation possible in order to 
 // keep your components up to date
 
-
 import * as React from 'react';
 import { AvailabilityDay } from './AvailabilityDay';
 import '../styles/App.css';
@@ -15,21 +14,26 @@ import { Day } from './AvailabilitySquare'
 import { ScheduleDay } from './ScheduleDay';
 import { CollapsibleHourShift } from './CollapsibleHourShift';
 
- // every component has a state object and props object
+interface SwitchView {
+  byWeekly: (ev: React.MouseEvent<HTMLButtonElement>) => void;
+  byDay: (ev: React.MouseEvent<HTMLButtonElement>) => void;
+} 
+
+// every component has a state object and props object
 //  Props don't change
 //  State starts with default value and mutates
-class App extends React.Component  <{}> {
+class App extends React.Component<SwitchView, {}> {
 
-  isWeekly: boolean;
-  constructor(props: any) {
-    super(props);
-    this.state = {isWeekly: true}
-    this.changeView = this.changeView.bind(this);
-  }
+  // isWeekly: boolean;
+  // constructor(props: any) {
+  //   super(props);
+  //   this.state = {isWeekly: true}
+  //   this.changeView = this.changeView.bind(this);
+  // }
   
-  changeView() {
-    this.setState({isWeekly: !this.isWeekly})
-  }
+  // changeView() {
+  //   this.setState({isWeekly: !this.isWeekly})
+  // }
   // changeDay() {
   //   this.setState({isWeekly: false})
   // }
@@ -56,14 +60,17 @@ class App extends React.Component  <{}> {
           <div className='verticalDivide'></div>
           <div className='rightContainer'>
           <h1> Schedule </h1>
-<div>
-          <button id='week' onClick={() => this.changeView()}>View Weekly
-          {!this.isWeekly && <div className='rightSide'></div>} 
-            </button>
-            <button id='day' onClick={() => this.changeView()}>View Day
-            {!this.isWeekly && <div className='kit'></div>} 
-            </button>
-     </div>   
+        
+ <form>
+           <button id='week' onClick={this.byWeekly}>
+            {/* {() => this.changeView()}>View Weekly */}
+           {/* {this.isWeekly && <div className='rightSide'></div>}  */}
+           {/* {this.isWeekly ? 'rightSide' : null} */}
+             </button>
+             <button id='day' onClick={() => this.changeView()}>View Day
+             {!this.isWeekly && <div className='kit'></div>} 
+             </button>
+ </form>   
             <div className='rightSide'>
               <ScheduleDay day={Day.Sunday}/>
               <ScheduleDay  day={Day.Monday}/>
@@ -71,13 +78,9 @@ class App extends React.Component  <{}> {
               <ScheduleDay  day={Day.Wednesday}/>
               <ScheduleDay  day={Day.Thursday}/>
               <ScheduleDay  day={Day.Friday}/>
-<<<<<<< HEAD
               </div>
             <div className='kit'>
-              <CollapsibleHourShift day={Day.Sunday}/>
-=======
               <CollapsibleHourShift d={new Date()}/>
->>>>>>> 58b2fdf377388e9d0c8a104c56d876f9a10cfe07
             </div>
             {/* <div><CollapsibleHourShift day={Day.Sunday}/></div> */}
           </div>
