88c5c5b1bcbde6d006f8dd8eb8a2dcf406844803
Brooks Townsend <brooksmtownsend@gmail.com>
2017-12-10 14:48:22 -0500

    Removed unfinished logic, fixed app css margins

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -28,23 +28,6 @@ class App extends React.Component  <{}, {isWeekly: boolean, style: {backgroundCo
                   style: {backgroundColor: 'lightskyblue'}
     }
     this.changeView = this.changeView.bind(this);
-    let schedule: JSON = require('../data/currentSchedule.json')
-
-    let shifts: any[] = schedule['shifts'] 
-    let days: any[] = [];
-
-    for (let i = 0; i < shifts.length; i++) {
-      console.log(shifts[0])
-      days[0] = shifts[0]
-    }
-
-    for (let i = 0; i < days.length; i++) {
-      for (let j = 0; j < days[i].length; j++) {
-        console.log(days[i][j].day)
-        console.log(days[i][j].hour)
-        console.log(days[i][j].onyens)
-      }
-    }
   }
   
   // if the week button is clicked and we are viewing by day then toggle to byweekly
