4505519227e63d3ecf1975189daeaf7756afbd51
Brooks Townsend <brooksmtownsend@gmail.com>
2017-12-10 14:52:04 -0500

    Better spacing

--- src/scripts/parseSchedule.ts
+++ src/scripts/parseSchedule.ts
@@ -1,16 +1,17 @@
 let schedule: JSON = require('../data/currentSchedule.json')
 
-let shifts: any[] = schedule.parse('shifts')
+let shifts: any[] = schedule['shifts'] 
 let days: any[] = [];
 
 for (let i = 0; i < shifts.length; i++) {
+  console.log(shifts[0])
   days[0] = shifts[0]
 }
 
 for (let i = 0; i < days.length; i++) {
-  for (let j = 0; j < days[0].length; j++) {
-    console.log(days[0].day)
-    console.log(days[0].hour)
-    console.log(days[0].onyens)
+  for (let j = 0; j < days[i].length; j++) {
+    console.log(days[i][j].day)
+    console.log(days[i][j].hour)
+    console.log(days[i][j].onyens)
   }
 }
